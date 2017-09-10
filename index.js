5c155343f9d31af5400558b80801a7386da49dfd
tariqibrahim <tariq1890@yahoo.in>
2017-09-09 22:42:15 -0700

    Added method 3

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 checksum.file('destiny_2.jpg', function (err, sum) {
-    console.log("Generated checksum from checksum package:", sum);
+    console.log("Generated checksum from checksum library:", sum);
 });
 
 fs.readFile('destiny_2.jpg', function (err, data) {
-    console.log("Genereated checksum 2 : ", calcChecksum('destiny_2.jpg'))
+    console.log("Genereated checksum 2 : ", calcChecksum2('destiny_2.jpg'))
 });
 
+calcChecksum3('destiny_2.jpg');
 
-function calcChecksum (str, algorithm, encoding) {
+function calcChecksum2 (str, algorithm, encoding) {
     return crypto
         .createHash(algorithm || 'md5')
         .update(str, 'utf8')
         .digest(encoding || 'hex')
+}
+
+function calcChecksum3(path) {
+    const filestream = fs.createReadStream(path);
+    const hash = crypto.createHash('sha256');
+    filestream.setEncoding('utf8');
+    filestream.on('data', function(data){
+           hash.update(data, 'utf8')
+    });
+    filestream.on('end', function () {
+        console.log("Checksum from method 3 : ", hash.digest('hex'))
+    })
 }
