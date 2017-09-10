511368a002c3d7b728b89127cc5db1490c34af8d
tariqibrahim <tariq1890@yahoo.in>
2017-09-09 22:54:03 -0700

    Added results as comments

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ checksum.file('brokenage-hero.png', function (err, sum) {
 });
 
 fs.readFile('brokenage-hero.png', function (err, data) {
-    console.log("Genereated checksum 2 : ", calcChecksum2(data))
+    console.log("Generated checksum 2 : ", calcChecksum2(data))
 });
 
 calcChecksum3('brokenage-hero.png');
@@ -30,3 +30,8 @@ function calcChecksum3(path) {
         console.log("Checksum from method 3 : ", hash.digest('hex'))
     })
 }
+
+//Results
+//Genereated checksum 2 :  f66318024c525e2f4719af8d3c4f4738
+//Checksum from method 3 :  8acbe571868f119520f00eb32631d098f55f5a0ddac1f62f11f592e0df78c10b
+//Generated checksum from checksum library: 8c2075377026ba3aec5bc5b573908b54a01e5800
