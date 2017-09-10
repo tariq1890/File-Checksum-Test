4e1fe4991064a63c0178c6c448756ee980262b9f
tibrahim <tibrahim@gaikai.com>
2017-09-09 22:31:24 -0700

    Added alternate method of checksum

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-var checksum = require('checksum');
+const checksum = require('checksum');
+const fs = require('fs');
+const crypto = require('crypto');
 
 checksum.file('destiny_2.jpg', function (err, sum) {
     console.log("Generated checksum from checksum package:", sum);
 });
 
+fs.readFile('destiny_2.jpg', function (err, data) {
+    console.log("Genereated checksum 2 : ", calcChecksum('destiny_2.jpg'))
+});
+
+
+function calcChecksum (str, algorithm, encoding) {
+    return crypto
+        .createHash(algorithm || 'md5')
+        .update(str, 'utf8')
+        .digest(encoding || 'hex')
+}
